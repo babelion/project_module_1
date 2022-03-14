4d1037233a133d7f7e25ab9dda10f480a893468a
Sergei Babkin <cmark.13.81.79.086@gmail.com>
2022-03-14 13:57:00 -0500

    script has been created

--- js/script.js
+++ js/script.js
@@ -11,7 +11,7 @@ let personalMovieDB = {
 let title_1 = prompt('Один из последних просмотренных фильмов?'),
     score_1 = prompt('На сколько оцените его?'),
     title_2 = prompt('Один из последних просмотренных фильмов?'),
-    score_2 = prompt('На сколько оцените его?')4
+    score_2 = prompt('На сколько оцените его?');
 
 
 personalMovieDB.movies[title_1] = score_1;
