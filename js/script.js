a3d64874e897be3721a5ee4c8545b693ddade1c9
Sergei Babkin <cmark.13.81.79.086@gmail.com>
2022-03-14 13:55:52 -0500

    script has been created

--- js/script.js
+++ js/script.js
@@ -1,4 +1,19 @@
-let number = 5;
-const leftBorderWidth = 1;
+let numberOfFilms = +prompt('Сколько фильмов вы уже посмотрели?');
 
-console.log(number);
+let personalMovieDB = {
+    count: numberOfFilms,
+    movies: {},
+    actors: {},
+    genres: [],
+    privat: false
+};
+
+let title_1 = prompt('Один из последних просмотренных фильмов?'),
+    score_1 = prompt('На сколько оцените его?'),
+    title_2 = prompt('Один из последних просмотренных фильмов?'),
+    score_2 = prompt('На сколько оцените его?')4
+
+
+personalMovieDB.movies[title_1] = score_1;
+personalMovieDB.movies[title_2] = score_2;
+
